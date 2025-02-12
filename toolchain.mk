8ae294665be03343cbf25e8c0dae9e6529afce89
FreshPenguin112 <93781766+FreshPenguin112@users.noreply.github.com>
2025-02-12 14:24:00 -0600

    Update toolchain.mk

--- toolchain.mk
+++ toolchain.mk
@@ -18,28 +18,28 @@ srcs = elf2flt linux uclibc-ng riscv-binutils-gdb riscv-gcc
 srcdirs = $(patsubst %,$(TOPDIR)/src/%,$(srcs))
 
 sources:$(srcdirs)
-	for d in $(srcdirs); do cd "$$d" && git pull; done
+	for d in $(srcdirs); do cd "$$d" && git pull --depth=1; done
 
 $(TOPDIR)/src/elf2flt:
-	cd $(TOPDIR)/src && git clone -b riscv https://github.com/chmmn/elf2flt.git
+	cd $(TOPDIR)/src && git clone --depth=1 -b riscv https://github.com/chmmn/elf2flt.git
 
 $(TOPDIR)/src/linux:
-	cd $(TOPDIR)/src && git clone -b riscv https://github.com/chmmn/linux.git
+	cd $(TOPDIR)/src && git clone --depth=1 -b riscv https://github.com/chmmn/linux.git
 
 $(TOPDIR)/src/uclibc-ng:
-	cd $(TOPDIR)/src && git clone -b riscv https://github.com/chmmn/uclibc-ng.git
+	cd $(TOPDIR)/src && git clone --depth=1 -b riscv https://github.com/chmmn/uclibc-ng.git
 
 $(TOPDIR)/src/riscv-binutils-gdb:
-	cd $(TOPDIR)/src && git clone https://github.com/riscv/riscv-binutils-gdb.git
+	cd $(TOPDIR)/src && git clone --depth=1 https://github.com/riscv/riscv-binutils-gdb.git
 
 $(TOPDIR)/src/riscv-gcc:
-	cd $(TOPDIR)/src && git clone https://github.com/riscv/riscv-gcc.git
+	cd $(TOPDIR)/src && git clone --depth=1 https://github.com/riscv/riscv-gcc.git
 
 binutils_config:
 	-mkdir -p $(TOPDIR)/build/binutils && cd $(TOPDIR)/build/binutils && $(TOPDIR)/src/riscv-binutils-gdb/configure --prefix=$(PREFIX) --target=$(TARGET) --with-sysroot=$(SYSROOT) --disable-multilib --disable-werror --disable-nls --with-expat=yes --disable-gdb
 
 binutils_build:
-	cd $(TOPDIR)/build/binutils && make -j10
+	cd $(TOPDIR)/build/binutils && make -j$(nproc)
 binutils_install:
 	cd $(TOPDIR)/build/binutils && make install
 
@@ -48,7 +48,7 @@ gcc1_config:
 	cd $(TOPDIR)/build/gcc1 && $(TOPDIR)/src/riscv-gcc/configure --prefix=$(PREFIX) --target=$(TARGET) --with-sysroot=$(SYSROOT) --with-newlib --without-headers --disable-shared --disable-threads --with-system-zlib --enable-tls --enable-languages=c --disable-libatomic --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-bootstrap  --enable-checking=yes --disable-multilib --with-abi=$(ABI) --with-arch=$(ARCH)
 
 gcc1_build:
-	cd $(TOPDIR)/build/gcc1 && make -j10
+	cd $(TOPDIR)/build/gcc1 && make -j$(nproc)
 gcc1_install:
 	cd $(TOPDIR)/build/gcc1 && make install
 
@@ -78,7 +78,7 @@ gcc2_config:
 	cd $(TOPDIR)/build/gcc2 && $(TOPDIR)/src/riscv-gcc/configure --prefix=$(PREFIX) --target=$(TARGET) --with-sysroot=$(SYSROOT) --disable-shared --disable-threads --with-system-zlib --enable-tls --enable-languages=c,c++ --disable-libatomic --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-bootstrap  --enable-checking=yes --disable-multilib --with-abi=$(ABI) --with-arch=$(ARCH) CFLAGS_FOR_TARGET="-Os -fPIC"
 
 gcc2_build:
-	cd $(TOPDIR)/build/gcc2 && make -j10
+	cd $(TOPDIR)/build/gcc2 && make -j$(nproc)
 gcc2_install:
 	cd $(TOPDIR)/build/gcc2 && make install
 
@@ -90,4 +90,3 @@ elf2flt_build:
 	cd $(TOPDIR)/build/elf2flt && make
 elf2flt_install:
 	cd $(TOPDIR)/build/elf2flt && make install prefix=$(PREFIX) && cp $(TOPDIR)/src/elf2flt/elf2flt-riscv.ld $(PREFIX)/$(TARGET)/lib/elf2flt.ld -a
-
